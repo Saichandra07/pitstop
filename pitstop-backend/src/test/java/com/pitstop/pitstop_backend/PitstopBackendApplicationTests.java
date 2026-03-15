38ab17ea12e9bea92acf1e89d9d80d932fefd1fa
Saichandra07 <thatikondasaichandra@gmail.com>
2026-03-16 01:46:49 +0530

    Initial project setup - Spring Boot Backend added

---
+++ pitstop-backend/src/test/java/com/pitstop/pitstop_backend/PitstopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitstop.pitstop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitstopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
