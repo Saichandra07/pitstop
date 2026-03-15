3b4b476258d13f643f086927f683ba1223e80a62
Saichandra07 <thatikondasaichandra@gmail.com>
2026-03-16 01:46:49 +0530

    Initial project setup - Spring Boot Backend added

---
+++ pitstop-backend/src/main/java/com/pitstop/pitstop_backend/PitstopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pitstop.pitstop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitstopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitstopBackendApplication.class, args);
+	}
+
+}
